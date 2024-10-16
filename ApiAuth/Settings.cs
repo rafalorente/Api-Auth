80028386af42e8c58052eeafefcb01e0e888aef1
Rafael Vieira Lorente <53351331+rafalorente@users.noreply.github.com>
2024-10-16 14:18:36 -0300

    fix: ajuste na secret key

--- ApiAuth/Settings.cs
+++ ApiAuth/Settings.cs
@@ -2,6 +2,8 @@
 {
     public static class Settings
     {
-        public static string Secret = "2443A206FD303A8AF83566C2A7809F3BB283D2AC9BE0E45363B25E65B3F945DC";
+        public static string Secret = "";
+
+        //sugestao de chave: 2443A206FD303A8AF83566C2A7809F3BB283D2AC9BE0E45363B25E65B3F945DC;
     }
 }
